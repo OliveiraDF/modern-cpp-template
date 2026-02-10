0d6f4f92e50a83ce665caff60334732698627c87
Olivier Choquet <48131940+OliveiraDF@users.noreply.github.com>
2026-02-10 11:48:10 +0100

    Commitar

--- cmake/uncrustify.cmake
+++ cmake/uncrustify.cmake
@@ -1,6 +1,7 @@
 find_program(UNCRUSTIFY_PATH uncrustify)
 
 if(UNCRUSTIFY_PATH)
+
     add_custom_target(
         format
         COMMAND find ${CMAKE_SOURCE_DIR}/src -type f \( -name "*.cpp" -o -name "*.h" \) -exec ${UNCRUSTIFY_PATH} -c ${CMAKE_SOURCE_DIR}/.uncrustify --no-backup --replace {} +
@@ -8,6 +9,9 @@ if(UNCRUSTIFY_PATH)
         COMMENT "Formatage du code avec Uncrustify"
         VERBATIM
     )
+
 else()
+
     message(WARNING "uncrustify non trouvé : la cible 'format' ne sera pas disponible")
+    
 endif()
